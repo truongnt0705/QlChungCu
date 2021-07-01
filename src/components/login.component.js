ed56fb56056df447f4f9b14e36acf0e2723bdad3
trongnt0705 <truontph07580@fpt.edu.vn>
2021-07-01 21:35:46 +0700

    gh

--- src/components/login.component.js
+++ src/components/login.component.js
@@ -1,20 +1,69 @@
 import React, { Component } from "react";
 
 export default class Login extends Component {
+
+   constructor(props){
+       super(props)
+       this.state={
+           "username":"",
+           "password":"",
+           islogin: localStorage.getItem("accessToken") !=null
+       }
+   }
+   setParams = (event)=>{
+       this.setState({[event.target.name]: event.target.value})
+   }
+   login = ()=>{
+       var myHeader = new Headers();
+       myHeader.append("Conten-Type","application/x-www-form-urlencoded");
+
+       var urlencoded = new URLSearchParams();
+       urlencoded.append("username", this.state.username);
+       urlencoded.append("password", this.state.password)
+      
+       var requesOptions = {
+       method: "POST",
+       headers: myHeader,
+       body: urlencoded,
+       redirect: 'follow'
+    };
+    fetch("https://learn-api.jmaster.io:8443/api/login",requesOptions)
+      .then(response => {
+              console.log(response)
+              if(response.ok){
+                return response.json()
+              }
+             throw Error(response.status)
+            })
+     
+      .then(result => {console.log(result)
+       localStorage.setItem("accessToken", result.accessToken)
+       alert("ok")
+       this.setState({islogin: true})
+    })
+      .catch(error =>{console.log('error', error)
+      alert("username, password are wrong")
+        });
+    }
+   onLogoutSuccess=()=>{
+       this.setState({islogin: false})
+   }
+   
     render() {
-        return (
+        return <div> {this.state.islogin ? 
+            <trangchu key={this.state.islogin} onLogoutSuccess={this.onLogoutSuccess}/>:
             <form>
 
                 <h3>Log in</h3>
 
                 <div className="form-group">
                     <label>Tên đăng nhập</label>
-                    <input type="email" className="form-control" placeholder="tendn" />
+                    <input type="email" name="username" className="form-control" placeholder="Enter Username" />
                 </div>
 
                 <div className="form-group">
                     <label>Mật khẩu</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
+                    <input type="password" name="password" className="form-control" placeholder="Enter password" />
                 </div>
 
                 <div className="form-group">
@@ -24,11 +73,12 @@ export default class Login extends Component {
                     </div>
                 </div>
 
-                <button type="submit" className="btn btn-dark btn-lg btn-block">Sign in</button>
+                <button type="submit" className="btn btn-dark btn-lg btn-block" onClick={this.login}>Sign in</button>
                 <p className="forgot-password text-right">
                     Forgot <a href="#">password?</a>
                 </p>
             </form>
-        );
+    }
+            </div>
     }
 }
